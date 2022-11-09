6c4b0be66795ca46a7e354207d3d899491f70acb
Hunter McVey <76709112+Moopsters@users.noreply.github.com>
2022-11-09 14:23:32 -0600

    Update ServerV1_U.py
    Additional framework modifications to match what the basic client will ask for.

--- UDP_v1/ServerV1_U.py
+++ UDP_v1/ServerV1_U.py
@@ -2,8 +2,13 @@
 First implementation of a UDP media server
 Ultimate goal will be to have a running server for users to request various forms of media downloads
 """
+"""
+First implementation of a UDP media server
+Ultimate goal will be to have a running server for users to request various forms of media downloads
+"""
 import socket           #import the required modules
 from threading import Thread
+import threading
 import cv2 as cv
 import pickle
 import hmac
@@ -13,18 +18,37 @@ HOST = "127.0.0.1"      #Host ip assignment
 PORT = 10597            #Random unused port
 MAXCLIENTS = 5
 
-currentUsers = {} 
+currentUsers = {}
+sendingMedia = []
+
+stop_event = threading.Event()   
 
 print("\nMy media server. Version One.\n")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)                       #create a socket named s using IPV4 with a UDP connection
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                     #set the socket to not block after a connection
+socketT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                       #create a socket named s using IPV4 with a UDP and TCP connection
+socketU = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+socketT.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)                     #set the socket to not block after a connection
+socketU.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  
 
-s.bind((HOST, PORT))                                                        #bind the socket using the host/port given       
-s.listen(MAXCLIENTS)                                                        #listen for the max amount of clients given
+socketT.bind((HOST, PORT))                                                        #bind the socket using the host/port given 
+socketU.bind((HOST, PORT))      
+socketT.listen(MAXCLIENTS)                                                        #listen for the max amount of clients given
+socketU.listen(MAXCLIENTS)
+
+def sendMedia():
+    global currentUsers                                                     #use the global logged in users dictionary
+    try:
+        while not stop_event:
+            client_socket, client_address = socketU.accept()
+            sendingMedia.append(client_socket)
+            print(f"Will send 'media' to {client_address} via UDP here")
+            stop_event.set()
+    except KeyboardInterrupt:                                               
+        pass
+    
 
 def listenForClients(cs):                                                   #define our thread function
-    global currentUsers                                                     #use the global logged in users dictionary
+    global currentUsers                                                     
     try:
         while True:                                                         #while the thread is running
             try:
@@ -35,26 +59,35 @@ def listenForClients(cs):                                                   #def
                     if client_socket == cs:                                 #find the connection of the thread
                         del currentUsers[userName]                          #remove them from the dictionary of logged in users
                 break
-            if userInput != []:                                             #if the user actually sent usable data
-                pass
+            if userInput != []:                                             #if the user actually sent data
+                match userInput[0]:
+                    case "Play":
+                        mediaThread.start()
+                    case _:
+                        print("User did not send usable data")
     except KeyboardInterrupt:                                               #stop the server with interrupt
         if currentUsers != {}:                                                  #close all the sockets
             for key, cs in currentUsers.copy().items():
                 cs.close()                                                         #close the socket
-        t.join()    
-        sys.exit()
         
+        chatThread.join()    
+        sys.exit()
+
 try:
     while True:                                                             #while there is a connection
-        client_socket, client_address = s.accept()                          #accept connections
+        client_socket, client_address = socketT.accept()                    #accept connections
 
         print(f"Connected by {client_address}")                             #maintain server log
 
-        t = Thread(target = listenForClients, args = (client_socket,))      #create thread with 'listenForClients" function and send socket info as arg
-        t.daemon = True                                                     #set thread to daemon
-        t.start()                                                           #start the thread
+        chatThread = Thread(target = listenForClients, args = (client_socket,))      #create thread with 'listenForClients" function and send socket info as arg
+        mediaThread = Thread(target = sendMedia)
+
+        chatThread.daemon = True                                                     #set thread to daemon
+        mediaThread.daemon = True
+
+        chatThread.start()                                                           #start the thread
 except KeyboardInterrupt:                                                   #if interrupted
     if currentUsers != {}:                                                  #close all the sockets
         for key, cs in currentUsers.copy().items():
             cs.close()
-    s.close()
+    socketT.close()
